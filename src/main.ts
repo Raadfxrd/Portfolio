6831db60add827dfed35837a8e54fad14ee26845
Raadfxrd <borysbabas@gmail.com>
2024-11-12 02:26:07 +0100

    Fixed tiny rendering issues.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import type { Router } from 'vue-router'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from './components/pages/HomePage.vue'
 import PortfolioPage from './components/pages/PortfolioPage.vue'
 import ContactPage from './components/pages/ContactPage.vue'
@@ -9,7 +9,7 @@ import ServicesPage from './components/pages/ServicesPage.vue'
 import AboutPage from './components/pages/AboutPage.vue'
 
 const router: Router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', component: HomePage, name: 'Home' },
     { path: '/portfolio', component: PortfolioPage, name: 'Portfolio' },
